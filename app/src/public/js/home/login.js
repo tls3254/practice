f90a6516933fcbfbb0805f025fa0e95eb87ccdc8
tls3254 <bikim9923@gmail.com>
2023-10-30 00:20:52 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,19 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),  //질의 선택자
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req ={
+        id : id.value,
+        psword: psword.value,
+    };
+    console.log(req);
+    
+ }
+
+
+//DOM //html의 존재하는 데이터들을 사용
