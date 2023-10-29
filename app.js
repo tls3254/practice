9ee496fb50c634a310e89a6fdfdbce5fc223baea
tls3254 <bikim9923@gmail.com>
2023-10-29 22:00:28 +0900

    깃 저장소 초기화

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//모듈
+const express = require("express"); //require명령어로 express라는 모듈 다운
+const app = express();  //app이라는 변수 안에 express를 실행 시킨다
+
+//라우팅
+const home = require("./routes/home"); //상대적으로 명시해줘야됨
+// 앱 세팅
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use("/" , home); //use->미들 웨어를 등록해주는 메서드.
+
+module.exports =app;
